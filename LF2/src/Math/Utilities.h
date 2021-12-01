9e7bb4470c04eda25a2de41540ac6607d60ba0e8
Azakme199 <priyanshukujur180@gmail.com>
2021-12-01 08:24:38 +0530

    Refactor UtilityClass.h -Separate out Line and Vector2

---
+++ LF2/src/Math/Utilities.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Line.h"
+#include "RealVector2D.h"
+#include <corecrt.h>
+
+#define Infinity RealVector2D(_CRT_INT_MAX, _CRT_INT_MAX);
+
+RealVector2D IntersectTwoLines(Line& l1, Line& l2);
+float LineDistanceFromPoint2d(const Line l, const RealVector2D& p);
+Line LineFromPoints(const RealVector2D& p1, const RealVector2D& p2);
+Line LineFromPointAndSlope(float slope, RealVector2D& p);
+bool ThreePointsCanIntersect(const RealVector2D& p1, const RealVector2D& p2, const RealVector2D& q);
--- LF2/src/bandit.h
+++ LF2/src/bandit.h
@@ -1,18 +1,18 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-#include "UtilityClass.h"
+#include "Math/Utilities.h"
 sf::Texture Loader;
 class Bandit {
 public:
-    Vector2 Position;
-    Vector2 Velocity;
+    RealVector2D Position;
+    RealVector2D Velocity;
     float MaxSpeed = 200;
     sf::Sprite IdleSprite[4];
     sf::Sprite CurrentSprite;
     int Up = 0, Down = 0, Right = 0, Left = 0;
     float MaxTime = 4, Time = 0;
     Bandit()
-        :Position(Vector2(0, 0)), Velocity(Vector2(10, 10)) {
+        :Position(RealVector2D(0, 0)), Velocity(RealVector2D(10, 10)) {
         for (int i = 0; i < 3; i++) {
             Loader.loadFromFile("src/Resource/Bandit.png", sf::IntRect(80 * i + 320, 0, 80, 80));
             IdleSprite[i].setTexture(Loader);
@@ -21,10 +21,10 @@ public:
     }
     void AddForce(const double dt) {
         if (Up + Down + Right + Left == 0) {
-            Velocity.x = 0; Velocity.y = 0;
+            Velocity.SetMagnitude(0);
             return;
         }
-        Vector2 Force((Right - Left) * 100, (Down - Up) * 100);
+        RealVector2D Force((Right - Left) * 100, (Down - Up) * 100);
         Velocity = Velocity + Force * dt * 500;
         if (Velocity.Magnitude() > MaxSpeed) {
             Velocity.SetMagnitude(MaxSpeed);
@@ -34,7 +34,7 @@ public:
         Position = Position + Velocity * dt;
     }
     void Animate(sf::RenderWindow& window) {
-        CurrentSprite.setPosition(sf::Vector2f(Position.x, Position.y));
+        CurrentSprite.setPosition(sf::Vector2f(Position.get_x(), Position.get_y()));
         window.draw(CurrentSprite);
     }
 };
